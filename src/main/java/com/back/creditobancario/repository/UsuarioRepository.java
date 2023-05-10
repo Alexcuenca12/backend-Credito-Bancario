8510d63da3f1e859a1b6ed8f694b2d9f764180c8
Alexis <mateosiavig@gmail.com>
2023-05-09 22:50:51 -0500

    Usuarios Cambios

--- src/main/java/com/back/creditobancario/repository/UsuarioRepository.java
+++ src/main/java/com/back/creditobancario/repository/UsuarioRepository.java
@@ -1,15 +1,17 @@
 package com.back.creditobancario.repository;
 
 import com.back.creditobancario.model.Usuario;
+import org.apache.catalina.User;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
 
-public interface UsuarioRepository extends CrudRepository <Usuario, Long> {
+public interface UsuarioRepository extends CrudRepository<Usuario, Long> {
     public Usuario findByUsername(String username);
 
     Boolean existsByUsername(String username);
+
     Usuario findByusernameAndPassword(String username, String password);
 
     // Metodos Query
@@ -19,5 +21,14 @@ public interface UsuarioRepository extends CrudRepository <Usuario, Long> {
     public boolean existsByPassword(String password);
 
     @Query(value = "Select * from usuario  where enabled = 1", nativeQuery = true)
-    List<Usuario>listarUsuario();
+    List<Usuario> listarUsuario();
+
+    @Query(value = "select r.rol_nombre, p.pers_correo from rol r join usuario u on u.rolid = r.rolid " +
+            "join persona p on p.persid= u.perid where u.usuario_username =?", nativeQuery = true)
+    List<Usuario> listarUsuarioRol(String username);
+
+    @Query(value = "select p.pers_nombres, p.pers_foto from persona p " +
+            "join usuario u on p.persid= u.perid where u.usuario_username= ?", nativeQuery = true)
+    List<Usuario> listarImg(String username);
+
 }
--- src/main/java/com/back/creditobancario/service/ServiciosImpl/UsuarioServiceImp.java
+++ src/main/java/com/back/creditobancario/service/ServiciosImpl/UsuarioServiceImp.java
@@ -7,6 +7,8 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Service;
 
+import java.util.List;
+
 @Service
 public class UsuarioServiceImp extends GenericServiceImpl<Usuario, Long> implements UsuarioService {
     @Autowired
@@ -20,6 +22,16 @@ public class UsuarioServiceImp extends GenericServiceImpl<Usuario, Long> impleme
     @Override
     public Usuario login(String username, String password) {
         return usuarioRepository.findByusernameAndPassword(username, password);
+    }
+
+    @Override
+    public List<Usuario> listarUsuarioRol(String username) {
+        return usuarioRepository.listarUsuarioRol(username);
+    }
+
+    @Override
+    public List<Usuario> listarImg(String username) {
+        return usuarioRepository.listarImg(username);
     }
 
     @Override
