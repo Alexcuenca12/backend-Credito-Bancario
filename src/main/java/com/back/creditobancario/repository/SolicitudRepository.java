a533ba8fec2f5cf42cec26fd5618f44c2994165a
Alexis <mateosiavig@gmail.com>
2023-05-21 09:33:11 -0500

    m

--- src/main/java/com/back/creditobancario/repository/SolicitudRepository.java
+++ src/main/java/com/back/creditobancario/repository/SolicitudRepository.java
@@ -14,17 +14,15 @@ public interface SolicitudRepository extends JpaRepository<Solicitud, Long> {
             "c.cred_monto,c.cred_fecha,s.soli_estado FROM solicitud s \n" +
             "join persona p on s.persid=p.persid\n" +
             "join usuario u on p.persid=u.persid\n" +
-            "join credito c on s.credid=c.credid\n" +
-            "join sucursal su on su.sucuid=u.sucursalid", nativeQuery = true)
+            "join credito c on s.credid=c.credid", nativeQuery = true)
     List<Solicitud> listarSolicitudesEstado();
 
     @Query(value = "SELECT s.soliid, p.per_cedula,p.pers_nombres||''||p.pers_apellidos as nombres,\n" +
-            "c.cred_monto,c.cred_fecha,s.soli_estado,su.sucuid \n" +
+            "c.cred_monto,c.cred_fecha,s.soli_estado\n" +
             "FROM solicitud s \n" +
             "join persona p on s.persid=p.persid\n" +
             "join usuario u on p.persid=u.persid\n" +
             "join credito c on s.credid=c.credid\n" +
-            "join sucursal su on su.sucuid=u.sucursalid\n" +
             "where u.usuario_username=?", nativeQuery = true)
     List<Solicitud> listarSolicitudesUsername(String username);
 
