bed3b66116a332c0ff6adf3f630d2dbaebe7e3b1
Adrisher <adrian.guerrero.est@tecazuay.edu.ec>
2023-05-27 14:32:06 -0500

    Antes de merge con main

--- src/main/java/com/back/creditobancario/controller/EmailController.java
+++ src/main/java/com/back/creditobancario/controller/EmailController.java
@@ -23,6 +23,7 @@ import java.util.HashMap;
 import java.util.Map;
 
 @RestController
+@CrossOrigin(origins = {"*"})
 @RequestMapping("/api")
 public class EmailController {
 
@@ -46,28 +47,21 @@ public class EmailController {
     }
 
 
-    @PostMapping("/sendEmailFile/{id}")
-    public ResponseEntity<?> requestEmailFile(@PathVariable Long id, @ModelAttribute EmailFileDTO emailFileDTO){
-        String emailRechazo;
-        ControlCredito controlCredito = controlCreditoService.findByIDSoli(id);
-        if (controlCredito == null){return new ResponseEntity<>(HttpStatus.NOT_FOUND);}
-        else{emailRechazo = controlCredito.getCorreoRechazo();}
-
-        String base64file = emailRechazo;
-        byte[] fileBytes = Base64Utils.decodeFromString(base64file);
-        File outputFile = new File("src/main/resources/files/Solicitud_Credito_Rechazado.pdf");
-        try (FileOutputStream outputStream = new FileOutputStream(outputFile)) {
-            outputStream.write(fileBytes);
-        } catch (FileNotFoundException e) {throw new RuntimeException(e);
-        } catch (IOException e) {throw new RuntimeException(e);}
-
+    @PostMapping("/sendMailFile")
+    public ResponseEntity<?> requestMailFile(@ModelAttribute EmailFileDTO emailFileDTO){
         try {
-            Path path = Paths.get("src/main/resources/files/Solicitud_Credito_Rechazado.pdf");
+            String fileName = emailFileDTO.getFile().getOriginalFilename();
+            Path path = Paths.get("src/main/resources/files/" + fileName);
+
+            Files.createDirectories(path.getParent());
+            Files.copy(emailFileDTO.getFile().getInputStream(), path, StandardCopyOption.REPLACE_EXISTING);
+
             File file = path.toFile();
             emailService.sendEmailWithFile(emailFileDTO.getToUser(), emailFileDTO.getSubject(), emailFileDTO.getMessage(), file);
+
             Map<String, String> response = new HashMap<>();
             response.put("estado", "Enviado");
-            response.put("archivo", "Solicitud_Credito_Rechazado.pdf");
+            response.put("archivo", fileName);
 
             return ResponseEntity.ok(response);
 
